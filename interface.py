a1a8414ac65b2d60e52751e85fad889aabb598cf
Matheus Velloso <matheuspvelloso@gmail.com>
2025-06-11 11:56:23 -0300

    add button for type of graphic and button for uploading file

--- interface.py
+++ interface.py
@@ -15,7 +15,7 @@ if "pagina" not in st.session_state:
 
 # Sidebar com botões
 st.sidebar.button("Início", on_click=lambda: st.session_state.update({"pagina": "Início"}))
-st.sidebar.button("Gráficos", on_click=lambda: st.session_state.update({"pagina": "Gráficos"}))
+st.sidebar.button("Gráfico", on_click=lambda: st.session_state.update({"pagina": "Gráfico"}))
 st.sidebar.button("Código", on_click=lambda: st.session_state.update({"pagina": "Código"}))
 
 
@@ -25,8 +25,17 @@ if st.session_state.pagina == "Início":
     st.subheader('Você poderá analisar com precisão dados do leitor de placas "Elisa" automaticamente')
     st.markdown("---", unsafe_allow_html=True)
 
-elif st.session_state.pagina == "Gráficos":
-    st.title("Gráficos")
+elif st.session_state.pagina == "Gráfico":
+    st.markdown("<h1 style='text-align: center;'>Gráfico</h1>", unsafe_allow_html=True)
+    st.markdown("---", unsafe_allow_html=True)
+    radio_btn = st.radio("Qual tipo de gráfico?", options=("Barra", "Linha"))    
+    file = st.file_uploader("**1)** Faça o upload de seu arquivo:", type=["xlsx"])
+    if radio_btn == "Barra":
+        st.markdown("Barra")
+    elif radio_btn == "Linha":
+        st.markdown("Linha")
+
+
 
 elif st.session_state.pagina == "Código":
     code = """
