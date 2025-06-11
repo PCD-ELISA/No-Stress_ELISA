6a202d603516b9223292a307ac8aa630ed61b843
Matheus Velloso <matheuspvelloso@gmail.com>
2025-06-11 09:36:45 -0300

    Criando interface com Streamlit

--- interface.py
+++ interface.py
@@ -1,2 +1,12 @@
-print("Essa é a interface")
-print("WIP!!!")
+import streamlit as sl
+sl.title("No-Stress Elisa")
+sl.subheader('Você poderá analisar com precisão dados do leitor de placas "Elisa" automaticamente')
+sl.markdown("---")
+
+
+#Mostrar código:
+code = """
+#Colocar o código aqui
+
+"""
+sl.code(code, language="python")
