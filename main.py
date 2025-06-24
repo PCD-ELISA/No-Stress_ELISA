25c6bb1b3d5a84b14fff4c68b7c80536f80530de
Matheus P. Velloso da Silveira <matheuspvelloso@gmail.com>
2025-06-24 14:37:14 -0300

    edit caption elisa image

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if st.session_state.pagina == "Início":
     with col1:
             st.image(
             "images/elisa.png",
-            caption="Leitor de placas ELISA moderno",
+            caption="Leitor de placas ELISA moderno / Disponível em: https://www.biosystemsimportadora.com.br/produtos/leitora-automatica-de-absorbancia-faixa-de-comprimento-de-onda-de-340-750-nm-disco-para-5-filtros-touchscreen-colorida-microplacas-6-a-384-pocos-agitacao-incubacao-ate-50c-software-gen5-biotek-800ts/",
             width=200
             )
     with col2:
